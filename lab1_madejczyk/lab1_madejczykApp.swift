780a06942b3cd8771966bfb22b72d787b147c401
Kacper Madejczyk <kacper.madejczyk@pollub.edu.pl>
2023-10-03 14:33:30 +0200

    Initial Commit

---
+++ lab1_madejczyk/lab1_madejczykApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab1_madejczykApp.swift
+//  lab1_madejczyk
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct lab1_madejczykApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
